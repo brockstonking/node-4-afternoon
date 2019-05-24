8b12c67e6c7c5277255006ce32635629e73eef7a
Brockston King <brockstonking@Brockstons-MacBook-Pro.local>
2019-05-24 06:58:05 -0600

    working, have completed up through step 9

---
+++ server/controllers/cartController.js
@@ -0,0 +1,41 @@
+const swag = require('./../models/swag');
+const users = require('./../models/users');
+
+module.exports = {
+    add: (req, res, next) => {
+        const { id } = req.params;
+        let { user } = req.session;
+
+        const itemInCart =  user.cart.findIndex( e => { return e.id == id })
+
+        if (itemInCart === -1){
+            const item = swag.find( e => { return e.id == id})
+
+            user.cart.push(item);
+            user.total += item.price;
+        } 
+
+        res.status(200).send(user);
+    },
+    delete: (req, res, next) => {
+        const { id } = req.params;
+        const { session } = req;
+
+        const itemIndex = session.user.cart.findIndex( e => e.id  == id);
+        const swagItem = swag.findIndex( e => e.id == id);
+
+        if (itemIndex !== -1) {
+            session.user.cart.splice(itemIndex, 1);
+            session.user.total -= swagItem.price;
+        }
+        res.status(200).send(session.user)
+    },
+    checkout: (req, res, next) => {
+        const { session } = req;
+
+        session.user.cart = [];
+        session.user.total = 0;
+
+        res.status(200).send(session.user)
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const checkForSession = require('./middlewares/checkForSession')
+const swagController = require('./controllers/swagController');
+const authController = require('./controllers/authController');
+const bodyParser = require('body-parser');
+const cartController = require('./controllers/cartController');
+
+const app = express();
+
+const { SERVER_PORT, SESSION_SECRET } = process.env;
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(checkForSession);
+
+app.get('/api/swag', swagController.read);
+
+app.post('/api/login', authController.login);
+app.post('/api/register', authController.register);
+app.post('/api/signout', authController.signOut);
+app.get('/api/user', authController.getUser);
+app.get('/api/users', authController.getAllUsers);
+
+
+app.post('/api/cart/checkout', cartController.checkout);
+app.post('/api/cart/:id', cartController.add);
+app.delete('/api/cart/:id', cartController.delete);
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Listening in on port ${ SERVER_PORT }`)
+})
